d3f4c54ade065151859539cbcd514cb069948575
gabteni <48386716+gabteni@users.noreply.github.com>
2024-05-18 21:57:02 +0100

    Update Classifier.py

--- skXCS/Classifier.py
+++ skXCS/Classifier.py
@@ -34,7 +34,7 @@ class Classifier:
         self.mass=0
     def match(self,state,xcs):
         mass=0
-        #dev=0
+        dev=0
         for i in range(len(self.condition)):
             specifiedIndex = self.specifiedAttList[i]
             attributeInfoType = xcs.env.formatData.attributeInfoType[specifiedIndex]
@@ -54,11 +54,16 @@ class Classifier:
                     return False
                 else:
                     return False
-            mass+=(((self.condition[i][0]+self.condition[i][1])/2)-instanceValue)**2
+            dev+=1
+            center=(self.condition[i][1]+self.condition[i][0])/2
+            d = abs(instanceValue - center)
+            L = abs(self.condition[i][1]-self.condition[i][0]) / 2
+            mass+= d / L
+            ########mass+=(((self.condition[i][0]+self.condition[i][1])/2)-instanceValue)**2
             #mass+=((((self.condition[i][0]+self.condition[i][1])/2)-instanceValue)**2)/((self.condition[i][1]-self.condition[i][0])**2)
             #dev+=self.condition[i][1]-self.condition[i][0]
-        self.mass=math.sqrt(mass)
-        #self.mass/=dev
+        #######self.mass=math.sqrt(mass)
+        self.mass=mass/dev
         #self.mass/=len(self.condition)
         return True
 
